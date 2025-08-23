cbe45513b6de3ea9d1306426f335c71c5ca2c5b5
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-08-23 21:36:58 +0530

    modified:   src/app/globals.css 	modified:   src/components/ai-elements/response.tsx

--- src/components/ai-elements/response.tsx
+++ src/components/ai-elements/response.tsx
@@ -12,48 +12,6 @@ export const Response = memo(
   ({ className, ...props }: ResponseProps) => (
     <Streamdown
       components={{
-        // pre: ({ node, className, children }) => {
-        //   let language = "javascript";
-
-        //   if (typeof node?.properties?.className === "string") {
-        //     language = node.properties.className.replace("language-", "");
-        //   }
-
-        //   let codeString = "";
-
-        //   // ✅ If children is a valid React element with props
-        //   if (
-        //     typeof children === "object" &&
-        //     children !== null &&
-        //     "props" in children
-        //   ) {
-        //     const c = children as { props: { children: string | string[] } };
-        //     codeString = Array.isArray(c.props.children)
-        //       ? c.props.children.join("")
-        //       : String(c.props.children);
-        //   } else {
-        //     // ✅ fallback: raw string, number, etc.
-        //     codeString = String(children ?? "");
-        //   }
-
-        //   return (
-        //     <ScrollArea className="w-[40vw]">
-        //       <CodeBlock
-        //         className={cn("my-4 h-auto w-[40vw]", className)}
-        //         code={codeString}
-        //         language={language}
-        //       >
-        //         <CodeBlockCopyButton
-        //           onCopy={() => console.log("Copied code to clipboard")}
-        //           onError={() =>
-        //             console.error("Failed to copy code to clipboard")
-        //           }
-        //         />
-        //       </CodeBlock>
-        //       <ScrollBar orientation="horizontal" />
-        //     </ScrollArea>
-        //   );
-        // },
         table: ({ className, ...props }) => (
           <ScrollArea className="overflow-x-auto border rounded-md max-w-[40vw]">
             <table className={cn("", className)} {...props} />
